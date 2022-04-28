1396daf3a9ee2dc6e02dd0af1a278e9f9dc65609
DownloadableFox <58178791+DownloadableFox@users.noreply.github.com>
2022-04-28 12:52:03 -0500

    Added dependencies install to VPS startup script.

--- Scripts/vps-setup.sh
+++ Scripts/vps-setup.sh
@@ -2,6 +2,20 @@
 # This script will setup a VPS with all the configurations I would normally use.
 # It will also install all the necessary software.
 
+# The packages to be installed are the following:
+# - git
+# - neovim
+# - zsh
+# - tmux
+# - python3
+# - python3-pip
+# - python3-dev
+# - nodejs
+# - npm
+# - nginx
+# - docker
+# - docker-compose
+
 USERNAME="fox"
 GITHUB="https://github.com/DownloadableFox"
 
@@ -11,9 +25,13 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
+echo "Creating user $USERNAME..."
+
 # Create user fox and add to sudoers
 useradd -m -s /bin/bash $USERNAME
 echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+echo "Setting up SSH..."
 
 # Create .ssh folder for the new user
 mkdir -p /home/$USERNAME/.ssh
@@ -41,3 +59,31 @@ sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd
 # Restart ssh service
 systemctl restart sshd
 
+echo "Downloading packages..."
+
+# If apt installed, install dependencies using apt
+if [ -x "$(command -v apt)" ]; then
+  apt update
+  apt upgrade
+  apt install -y git neovim zsh tmux python3 python3-pip python3-dev nodejs npm nginx docker docker-compose
+fi
+
+# If dnf is installed, install dependencies using dnf
+if [ -x "$(command -v dnf)" ]; then
+  dnf update
+  dnf upgrade
+  dnf install -y git neovim zsh tmux python3 python3-pip python3-devel nodejs npm nginx docker docker-compose
+fi
+
+# If pacman is installed, install dependencies with pacman
+if [ -x "$(command -v pacman)" ]; then
+  pacman -Syu
+  pacman -S --noconfirm git neovim zsh tmux python3 python3-pip python3-devel nodejs npm nginx docker docker-compose
+fi
+
+# Setting up zsh as default shell
+chsh -s /bin/zsh $USERNAME
+
+# Rebooting the system
+echo "Rebooting the system..."
+systemctl reboot
